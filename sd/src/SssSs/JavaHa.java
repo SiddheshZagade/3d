efc34a024f045a63abd49900818a4d00d6fdc192
SiddheshZagade <siddeshzagade7@gmail.com>
2020-09-16 21:14:09 +0530

    si

---
+++ sd/src/SssSs/JavaHa.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package SssSs;
+
+/**
+ * @author sid
+ *
+ */
+public class JavaHa {
+
+	/**
+	 * 
+	 */
+	public JavaHa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(9);
+		// TODO Auto-generated method stub
+
+	}
+
+}
